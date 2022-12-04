f4d9802259f54dcef434250cd76107eb8ada7cbe
Raphael Francisco Alves <raphaelbatealves@gmail.com>
2022-12-04 12:57:49 -0300

    Initial

---
+++ ControleDeEstoque/Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ControleDeEstoque.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        [Display(Name = "Produzido")]
+        public int QntProduzida { get; set; }
+        [Display(Name = "Vendido")]
+        public int QntVendida { get; set; }
+        [Display (Name= "Estoque")]
+        public int QntEstoque
+        {
+            get { return QntProduzida - QntVendida; }
+            set { return; }
+        }
+    }
+}
