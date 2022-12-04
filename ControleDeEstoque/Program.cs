dd84db52876731e9388df7a92e4513653067e44a
Raphael Francisco Alves <raphaelbatealves@gmail.com>
2022-12-04 16:36:00 -0300

    Bando de dados Azure

--- ControleDeEstoque/Program.cs
+++ ControleDeEstoque/Program.cs
@@ -24,8 +24,8 @@ namespace ControleDeEstoque
                 SeedData.Initialize(services);
             }
 
-                // Configure the HTTP request pipeline.
-                if (!app.Environment.IsDevelopment())
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
                 {
                     app.UseExceptionHandler("/Home/Error");
                     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
